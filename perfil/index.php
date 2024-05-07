1b8b37ccebaf571c9aae80429d2900e03ee12c3a
mauricioyan2019 <56656062+mauricioyan2019@users.noreply.github.com>
2024-05-07 18:33:39 -0300

    modifique para que busque por dni y devuelva correctamente los datos

--- perfil/index.php
+++ perfil/index.php
@@ -7,13 +7,24 @@
  if( isset($_POST['dni']) && !empty($_POST['dni']) )
  {
     $dni=(int)$_POST['dni'];
-    $registros= $objeto->obtenerPersona($dni);
+    $registros= $objeto->obtenerAlumnoCursos($dni);
    foreach($registros as $item)
    {
     $id_persona= $item['id'];
     $apellidonombre=$item['apellidonombre'];
     $dni=$item['dni'];
    }
+if (empty($registros)){
+   ?>      
+       <div class="alert alert-block alert-error fade in" style="max-width: 220px; margin: 0px auto 20px;">
+       <button data-dismiss="alert" class="close" type="button">×</button>
+       Sin Datos.............
+       </div> 
+  <?php
+  echo "<script>alert('Con el DNI no encontre ningun alumno....');</script>";
+  die();
+}
+
 ?>
 
 <div id="main">
@@ -34,12 +45,81 @@
              <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                <ol class="breadcrumb">
                  <li class="breadcrumb-item"><a href="../panelcontrol/index.html">Panel de Control</a></li>
-                 <li class="breadcrumb-item active" aria-current="page">Personas</li>
+                 <li class="breadcrumb-item active" aria-current="page">Alumno</li>
                </ol>
              </nav>
            </div>
          </div>
        </div>
+
+<!--seccion cursos-->
+
+        <section class="section">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Listado de Cursos</h4>
+                    <h5 class="card-title"><?php echo $item['apellidonombre']; ?></h5>
+                    <h5 class="card-title"><?php echo $item['dni']; ?></h5>
+                </div>
+                <div class="card-body">
+                    <!--- inico contenido -------------------------------------------------------------------------->
+                    <!--div class="buttons">
+                        <a href="nuevo.php" class="btn btn-outline-primary">Agregar</a>
+                    </div-->
+                    <table class="table table-flush" id="datatable">
+                        <thead class="thead-light">
+                            <tr>
+                                <th>#</th>
+                                <!--th>DNI</th>
+                                <th>Apellido Nombre</th-->
+                                <th>Curso</th>
+                                <th>Estado</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Item -->
+                            <?php
+                            $objeto = new Perfil();
+                            $cursos= $objeto->obtenerAlumnoCursos($dni);
+                            foreach ($cursos as $item) {
+                            ?>
+                                <tr>
+                                    <td><?php echo $item['id']; ?></td>
+                                    <td><?php echo $item['carrera']; ?></td>
+                                    <td><?php echo $item['estado']; ?></td>
+                                    <td>
+                                        <div class="buttons" style="margin:0 auto">
+
+                                            <!--form method="POST" role="form" action="editar.php" style="margin:0 auto">
+                                                <input type="hidden" name="id" value="<?php echo $item['id']; ?>">
+                                                <button class="btn btn-outline-primary">Editar</button>
+
+                                            </form-->
+
+                                            <form method="POST" role="form" action="../alumno/alumno_carrera_cuotas.php" style="margin:0 auto">
+                                                <input type="hidden" name="id" value="<?php echo $item['id']; ?>">
+                                                <button class="btn btn-outline-primary">Cuotas</button>
+
+                                            </form>
+                                        </div>
+
+                                    </td>
+                                </tr>
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                    <!--- fin contenido------------------------------------------------------- -->
+                </div>
+            </div>
+        </section>
+
+
+<!--fin seccion cursos-->
+
+
 
        <section class="section">
          <div class="card">
@@ -52,32 +132,32 @@
             
   <div class="col-md-8 mb-3">
     <label class="form-label">DNI</label>
-    <input name="dni"  class="form-control" type="text" tabindex="1" required value="<?php echo utf8_encode($item['dni']); ?>"/>
+    <input name="dni" disabled  class="form-control" type="text" tabindex="1" required value="<?php echo utf8_encode($item['dni']); ?>"/>
   </div>
   <input type="hidden" name="id_persona" value="<?php echo $item['id']; ?>">
   <div class="col-md-8 mb-3">
     <label class="form-label">Apellido y Nombre</label>
-    <input name="apellido_nombre"  class="form-control" type="text" tabindex="2" required value="<?php echo utf8_encode($item['apellidonombre']); ?>"/>
+    <input name="apellido_nombre" disabled class="form-control" type="text" tabindex="2" required value="<?php echo utf8_encode($item['apellidonombre']); ?>"/>
   </div>
 
   <div class="col-md-8 mb-3">
     <label class="form-label" >Domicilio </label>
-    <input name="domicilio"  class="form-control" type="text" tabindex="3" required value="<?php echo utf8_encode($item['domicilio']); ?>"/>
+    <input name="domicilio" disabled  class="form-control" type="text" tabindex="3" required value="<?php echo utf8_encode($item['domicilio']); ?>"/>
   </div>
 
   <div class="col-md-8 mb-3">
     <label class="form-label">Celular Principal</label>
-    <input name="cel_prin"  class="form-control" type="text" tabindex="4"  value="<?php echo utf8_encode($item['cel1']); ?>" />
+    <input name="cel_prin" disabled  class="form-control" type="text" tabindex="4"  value="<?php echo utf8_encode($item['cel1']); ?>" />
   </div>
 
   <div class="col-md-8 mb-3">
     <label class="form-label">Celular Alternativo</label>
-    <input name="cel_alte"  class="form-control" type="text" tabindex="4"  value="<?php echo utf8_encode($item['cel2']); ?>" />
+    <input name="cel_alte" disabled class="form-control" type="text" tabindex="4"  value="<?php echo utf8_encode($item['cel2']); ?>" />
   </div>  
 
   <div class="col-md-8 mb-3">
     <label class="form-label">Mail</label>
-    <input name="mail"  class="form-control" type="email" tabindex="4"  value="<?php echo utf8_encode($item['mail']); ?>" />
+    <input name="mail" disabled class="form-control" type="email" tabindex="4"  value="<?php echo utf8_encode($item['mail']); ?>" />
   </div>   
    
   
--- perfil/perfil.php
+++ perfil/perfil.php
@@ -15,6 +15,37 @@ class Perfil
             return $rs;
     }
 
+  public function obtenerAlumnoCursos($dni)
+  {
+    $data = array();
+    $consulta = "SELECT
+    persona.`apellidonombre` AS apellidonombre,
+    persona.`dni` AS dni,
+    alumno.`edad` AS edad,
+    alumno.`estado` AS estado,
+    carrera.`nombre` AS carrera,
+    alumno.`id` AS id,
+    persona.`domicilio` AS domicilio,
+    persona.`cel1` AS cel1,
+    persona.`cel2` AS cel2,
+    persona.`mail` AS mail
+
+    FROM
+        `alumno` 
+        INNER JOIN `carrera` 
+            ON (`alumno`.`carrera_id` = `carrera`.`id`)
+        INNER JOIN `persona` 
+            ON (`alumno`.`persona_id` = `persona`.`id` and `persona`.`id`>1 and `persona`.`dni`=$dni );";
+    $rs = mysqli_query(conexion::obtenerInstancia(), $consulta);
+    if (mysqli_num_rows($rs) > 0) {
+      while ($fila = mysqli_fetch_assoc($rs)) {
+        $data[] = $fila;
+      }
+    }
+    return $data;
+  }
+
+
     public function buscarDNI($dni)
     {
         $data = array();
