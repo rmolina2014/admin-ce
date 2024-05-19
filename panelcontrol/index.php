de24360763124b68d29ea3e34c7e0e003958f55e
roberto <molinaroberto.cgp@gmail.com>
2024-05-18 21:21:08 -0300

    cambiar los select por radibutton

--- panelcontrol/index.php
+++ panelcontrol/index.php
@@ -38,7 +38,7 @@ include("../menu.php");
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, commodi? Ullam quaerat similique iusto
                     temporibus, vero aliquam praesentium, odit deserunt eaque nihil saepe hic deleniti? Placeat delectus
                     quibusdam ratione ullam!
-                </div-->
+                </div--->
             </div>
         </section>
     </div>
